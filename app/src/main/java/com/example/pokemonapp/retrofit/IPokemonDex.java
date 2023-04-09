0e9e0c12b534366a93cb15c0cd2bda8e1da6a18e
zakaria0101 <zraissi403@gmail.com>
2023-04-09 22:15:58 +0000

    last commit ✅

---
+++ app/src/main/java/com/example/pokemonapp/retrofit/IPokemonDex.java
@@ -0,0 +1,11 @@
+package com.example.pokemonapp.retrofit;
+
+import com.example.pokemonapp.model.Pokedex;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+public interface IPokemonDex {
+    @GET("pokedex.json")
+    Observable<Pokedex> getListPokemon();
+}
